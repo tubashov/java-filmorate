aaeee3822c9cf547a3acaf9e97b7680f20a35eb8
tubashov <tubashov@yandex.ru>
2025-08-13 12:30:09 +0300

    Добавление логирования в класс GlobalExceptionHandler, класса NotFoundException, добавление аннотации @Positive

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
