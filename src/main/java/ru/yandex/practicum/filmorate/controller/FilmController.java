a98b57d5b13cdbe9a641ea349fb4be164b055a5c
tubashov <tubashov@yandex.ru>
2025-08-20 22:36:26 +0300

    Migrate clean changes from add-friends-likes excluding ignored/binary files

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,49 +1,74 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.*;
+import java.util.List;
 
-@Slf4j
 @RestController
 @RequestMapping("/films")
+@Validated // Включает проверку параметров методов (например, @Positive)
 public class FilmController {
 
-    private final Map<Integer, Film> films = new HashMap<>();
-    private int nextId = 1;
+    private final FilmService filmService;
 
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    // Добавление нового фильма
     @PostMapping
-    public Film create(@RequestBody @Valid Film film) {
-        // @Valid поручает Spring автоматически проверить поля объекта Film согласно аннотациям в классе
-        log.info("Получен запрос на добавление фильма: {}", film);
-
-        film.setId(nextId++);
-        films.put(film.getId(), film);
-        log.info("Фильм добавлен: {}", film);
-        return film;
+    public ResponseEntity<Film> addFilm(@RequestBody @Valid Film film) {
+        Film createdFilm = filmService.addFilm(film);
+        return new ResponseEntity<>(createdFilm, HttpStatus.CREATED);
     }
 
+    // Обновление существующего фильма
     @PutMapping
-    public Film update(@RequestBody @Valid Film film) {
-        log.info("Получен запрос на обновление фильма: {}", film);
-
-        if (!films.containsKey(film.getId())) {
-            log.warn("Попытка обновления несуществующего фильма с ID: {}", film.getId());
-            throw new ValidationException("Фильм с ID " + film.getId() + " не найден");
-        }
-
-        films.put(film.getId(), film);
-        log.info("Фильм обновлён: {}", film);
-        return film;
+    public ResponseEntity<Film> updateFilm(@RequestBody @Valid Film film) {
+        Film updatedFilm = filmService.updateFilm(film);
+        return ResponseEntity.ok(updatedFilm);
     }
 
+    // Получение фильма по ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> getFilmById(@PathVariable @Positive int id) {
+        Film film = filmService.getFilmById(id);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    // Получение всех фильмов
     @GetMapping
-    public Collection<Film> findAll() {
-        log.info("Запрошен список всех фильмов. Всего: {}", films.size());
-        return films.values();
+    public ResponseEntity<List<Film>> getAllFilms() {
+        return ResponseEntity.ok(filmService.getAllFilms());
+    }
+
+    // Поставить лайк фильму
+    @PutMapping("/{id}/like/{userId}")
+    public ResponseEntity<Void> addLike(@PathVariable @Positive int id,
+                                        @PathVariable @Positive int userId) {
+        filmService.addLike(id, userId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // Удалить лайк с фильма
+    @DeleteMapping("/{id}/like/{userId}")
+    public ResponseEntity<Void> removeLike(@PathVariable @Positive int id,
+                                           @PathVariable @Positive int userId) {
+        filmService.removeLike(id, userId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // Получить топ популярных фильмов (по умолчанию 10)
+    @GetMapping("/popular")
+    public ResponseEntity<List<Film>> getPopularFilms(@RequestParam(defaultValue = "10") @Positive int count) {
+        List<Film> films = filmService.getTopPopularFilms(count);
+        return new ResponseEntity<>(films, HttpStatus.OK);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserStorage {
+
+    User addUser(User user);
+
+    User updateUser(User user);
+
+    void removeUser(int userId);
+
+    Optional<User> getUserById(int id);
+
+    List<User> getAllUsers();
+}
