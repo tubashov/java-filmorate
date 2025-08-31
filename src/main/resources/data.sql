ea992515149440006d8470ba27f8bcec465f22c9
tubashov <tubashov@yandex.ru>
2025-08-31 14:15:25 +0300

    Добавление файла data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- MPA RATINGS
 MERGE INTO mpa_rating (id, name) KEY(id) VALUES (1, 'G');
 MERGE INTO mpa_rating (id, name) KEY(id) VALUES (2, 'PG');
@@ -12,3 +13,21 @@ MERGE INTO genres (id, name) KEY(id) VALUES (3, 'Мультфильм');
 MERGE INTO genres (id, name) KEY(id) VALUES (4, 'Триллер');
 MERGE INTO genres (id, name) KEY(id) VALUES (5, 'Документальный');
 MERGE INTO genres (id, name) KEY(id) VALUES (6, 'Боевик');
+=======
+-- MPA
+MERGE INTO mpa_rating (id, name) KEY(id) VALUES
+(1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17');
+
+-- GENRES
+MERGE INTO genres (id, name) KEY(id) VALUES
+(1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик');
+>>>>>>> aaa7374 (Добавление файла data.sql)
