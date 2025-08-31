76987a539a6c2d6350a37827605c979a4300b517
tubashov <tubashov@yandex.ru>
2025-08-31 18:23:56 +0300

    Добавление FilmorateApplicationTest.java, изменение getUserById-findUserById

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.dao.UserDbStorage;
 
-@SpringBootTest
-class FilmorateApplicationTests {
+import java.util.Optional;
 
-	@Test
-	void contextLoads() {
-	}
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({UserDbStorage.class})
+class FilmoRateApplicationTests {
+    private final UserDbStorage userStorage;
 
+    @Test
+    public void testFindUserById() {
+
+        Optional<User> userOptional = userStorage.findUserById(1);
+
+        assertThat(userOptional)
+                .isPresent()
+                .hasValueSatisfying(user ->
+                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+                );
+    }
 }
