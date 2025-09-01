cdff777d86338127ced11d7d462c28a618427031
tubashov <tubashov@yandex.ru>
2025-09-01 10:47:45 +0300

    Изменение FilmorateApplicationTest

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.dao.UserDbStorage;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @JdbcTest
@@ -17,17 +18,28 @@ import java.util.Optional;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({UserDbStorage.class})
 class FilmoRateApplicationTests {
+
     private final UserDbStorage userStorage;
 
     @Test
     public void testFindUserById() {
+        // 1. Создаём пользователя и сохраняем в БД
+        User newUser = new User();
+        newUser.setEmail("test@example.com");
+        newUser.setLogin("testUser");
+        newUser.setName("Test User");
+        newUser.setBirthday(LocalDate.of(1990, 1, 1));
 
-        Optional<User> userOptional = userStorage.findUserById(1);
+        User savedUser = userStorage.addUser(newUser);
 
+        // 2. Получаем пользователя по ID
+        Optional<User> userOptional = userStorage.findUserById(savedUser.getId());
+
+        // 3. Проверяем
         assertThat(userOptional)
                 .isPresent()
                 .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+                        assertThat(user).hasFieldOrPropertyWithValue("id", savedUser.getId())
                 );
     }
 }
