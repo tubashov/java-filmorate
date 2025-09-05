0adb807f4b036910b1607c8cabcf0a1e31186a14
tubashov <tubashov@yandex.ru>
2025-09-05 21:47:42 +0300

    Commit all files

--- src/main/java/ru/yandex/practicum/filmorate/model/FriendshipStatus.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.practicum.filmorate.model;
-
-public enum FriendshipStatus {
-    PENDING,    // неподтверждённая, запрос отправлен
-    CONFIRMED   // подтверждённая, дружба согласована
-}
