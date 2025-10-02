5b0f4b04382db96bef006f23fbda52f5361a635e
tubashov <tubashov@yandex.ru>
2025-10-02 16:33:55 +0300

    Correction LoggingAspect.java

--- src/main/java/ru/yandex/practicum/filmorate/logging/LoggingAspect.java
+++ src/main/java/ru/yandex/practicum/filmorate/logging/LoggingAspect.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.logging;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
